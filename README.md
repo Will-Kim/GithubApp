5d147f5d616dfbf07102c618b16a7d4d59c4ec15
ipapaya <ipapaya2@gmail.com>
2022-08-26 09:37:34 +0900

    env: java version 11 & binding 적용
    환경변수 적용
    Readme에 환경변수 적용 내용 변경

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # GithubApp
  show github issues with github api
+
+## Environment
+ - Java Version 11
+ - viewBinding true
+ - dataBinding true
+
---
+++ app/src/main/java/com/talesapp/githubapp/MainViewModel.kt
@@ -0,0 +1,10 @@
+package com.talesapp.githubapp
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+
+    init {
+
+    }
+}
