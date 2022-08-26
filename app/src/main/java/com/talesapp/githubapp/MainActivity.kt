9633ac0daa6665c0c414b390030ed03ed8df3d53
ipapaya <ipapaya2@gmail.com>
2022-08-26 14:28:59 +0900

    feat: SQLite DB Helper added
    DB Helper 추가
    SharedPreference 추가
    Statics Companion Object 추가

--- app/src/main/java/com/talesapp/githubapp/MainActivity.kt
+++ app/src/main/java/com/talesapp/githubapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.talesapp.githubapp
 
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.talesapp.githubapp.databinding.ActivityMainBinding
@@ -7,12 +8,15 @@ import com.talesapp.githubapp.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var viewModel: MainViewModel
+    lateinit var dbHelper : DBHelper
+    lateinit var database : SQLiteDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         viewModel = MainViewModel()
         setContentView(binding.root)
+
     }
 
     override fun onResume() {
